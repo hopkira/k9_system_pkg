5d55e44343e91fa901af1ed0f029a79530e850ed
Richard Hopkins <hopkira@googlemail.com>
2025-08-19 18:17:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+            ['resources/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'resource'), glob('resource/*')),
+        (os.path.join('share', package_name, 'resources'), glob('resources/*')),
         (os.path.join('share', package_name, 'assets'), glob('assets/*')),
     ],
     install_requires=['setuptools'],
