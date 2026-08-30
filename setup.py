5c6dfcd9a37bd0720baaa86c0f20599a97c7673d
Richard Hopkins <hopkira@googlemail.com>
2026-08-30 14:50:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
             'weather = k9_system_pkg.weather:main',
             'garden = k9_system_pkg.gardentasks:main',
             'k9_stt = k9_system_pkg.k9_stt_ros2:main',
-            'face_detect = k9_system_pkg.face_detection:main',
             'logo_test = k9_system_pkg.k9_logo_test:main',
             'intent = k9_system_pkg.intent:main',
             'conversation = k9_system_pkg.conversation_node:main',
